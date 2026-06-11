490a9424334942c61da3c534818cf8e49fbd5827
ebalyan <ebalyan@usc.edu>
2026-06-10 21:56:29 -0700

    Update util.h

--- util.h
+++ util.h
@@ -13,18 +13,25 @@
 template <typename T>
 std::set<T> setIntersection(std::set<T>& s1, std::set<T>& s2)
 {
+  std::set<T> result;
 
-
-
-
+  for(typename std::set<T>::iterator it = s1.begin(); it != s1.end(); ++it){
+    if(s2.find(*it) != s2.end()) {
+      result.insert(*it);
+    }
+  }
+result resukt;
 
 }
 template <typename T>
 std::set<T> setUnion(std::set<T>& s1, std::set<T>& s2)
 {
+  std::set<T> result = s1;
 
-
-
+  for(typename std::set<T>::iterator it = s2.begin(); it != s2.end(); ++it) {
+    result.insert(*it);
+  }
+  result result;
 
 
 }
